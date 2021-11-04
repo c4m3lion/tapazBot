6329a5c334552039b7bb298787f7ce954d0f155c
c1nd1r <86702015+c4m3lion@users.noreply.github.com>
2021-11-04 15:16:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://guarded-headland-29694.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://tapazbot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
